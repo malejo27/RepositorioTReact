4a073067fa0f380fff9e820b93d187c2f98d3c80
“estephany2828” <ejargoty@unicauca.edu.co>
2021-11-30 00:11:16 -0500

    obtener usuario con graphql

--- index.js
+++ index.js
@@ -3,27 +3,23 @@
 const { graphql, buildSchema } = require("graphql"); //primer  tipo de consulta squema
 const express = require("express"); //contruir el servicio donde voy  a consumir la consulta
 const { graphqlHTTP } = require("express-graphql"); //el point donde voy a hacer las peticiones
+//libreria de manejor de archivos
+const {readFileSync} = require("fs") 
+const {join} = require("path")
+const resolvers = require("./lib/resolvers") //traer un archivo js con una funcion
+
 
 const app = express(); //llamando el servicio configurando un server
 const port = process.eventNames.port || 3000; //debo darle un puerto para escuchar las peticiones
 
-//primera schema
-const schema = buildSchema(`
-type Query{
-    hello: String
-    saludo: String
-}
-`);
+//primera schema colocamos la ruta 
+const schema = buildSchema(
+    readFileSync(
+        join(__dirname, 'lib', 'schema_graphql'), //ruta donde esta el archivo 
+        'utf-8'
+    )
+);
 
-//funciones de hello y saludo
-const resolvers ={
-    hello:()=>{
-        return 'Hola  mundo'
-    },
-    saludo:()=>{
-        return 'hola a todos'
-    }
-}
 
 //configurar la aplicacion para cuando se lance, la ruta relativa, llamo al middleware y
 // pasamos los tres argumentos: schema, rootValue, graphiql (aplicacion grafixa)
