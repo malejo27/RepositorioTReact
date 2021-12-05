3637045004dd0f4d3e33fa92b0e38207e205b139
johana4rgoty <ejargoty@unicauca.edu.co>
2021-12-04 23:55:01 -0500

    presentar al sprint3

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 "use strict";
 
-require('dotenv').config()
-//const { graphql, buildSchema } = require("graphql"); //primer  tipo de consulta squema
-const {makeExecutableSchema}= require('graphql-tools') //proyectos grandes
-const express = require("express") //contruir el servicio donde voy  a consumir la consulta
-const { graphqlHTTP } = require("express-graphql") //el point donde voy a hacer las peticiones
-//libreria de manejor de archivos
-const {readFileSync} = require("fs") 
+//require('dotenv').config()
+const { graphql, buildSchema } = require("graphql"); //primer  tipo de consulta squema
+//const {makeExecutableSchema}= require('graphql-tools') 
+ const express = require("express") 
+ const { graphqlHTTP } = require("express-graphql") //el point donde voy a hacer las peticiones
+// //libreria de manejor de archivos
+ const {readFileSync} = require("fs") 
 const {join} = require("path")
 const resolvers = require("./lib/resolvers") //traer un archivo js con una funcion
 
 
-const app = express(); //llamando el servicio configurando un server
-const port = process.eventNames.port || 3000; //debo darle un puerto para escuchar las peticiones
+ const app = express(); //llamando el servicio configurando un server
+ const port = process.eventNames.port || 3000; //debo darle un puerto para escuchar las peticiones
+
+ const schema = buildSchema(
+    readFileSync(
+        join(__dirname, 'lib', 'schema.graphql'), //ruta donde esta el archivo 
+        'utf-8'
+    )
+);
 
 
-const typeDefs = readFileSync(
-    join(__dirname, 'lib', 'schema.graphql'),
-    'utf-8'
-)
-//llamo a la dependencia
-const schema = makeExecutableSchema({
-    typeDefs, resolvers})
+// const typeDefs = readFileSync(
+//     join(__dirname, 'lib', 'schema.graphql'),
+//     'utf-8'
+// )
+// //llamo a la dependencia
+// const schema = makeExecutableSchema({
+//     typeDefs, resolvers})
 
-//configurar la aplicacion para cuando se lance, la ruta relativa, llamo al middleware y
-// pasamos los tres argumentos: schema, rootValue, graphiql (aplicacion grafixa)
+// //configurar la aplicacion para cuando se lance, la ruta relativa, llamo al middleware y
+// // pasamos los tres argumentos: schema, rootValue, graphiql (aplicacion grafixa)
 
 app.use('/api', graphqlHTTP({
     schema: schema, 
