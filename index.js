dfb0ca3a41af6d96a9217267bbc41331d7c08c55
johana4rgoty <ejargoty@unicauca.edu.co>
2021-12-06 23:59:10 -0500

    query cpnsultar por id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 //require('dotenv').config()
 const { graphql, buildSchema } = require("graphql"); //primer  tipo de consulta squema
-//const {makeExecutableSchema}= require('graphql-tools') 
+const {makeExecutableSchema}= require('graphql-tools') 
  const express = require("express") 
  const { graphqlHTTP } = require("express-graphql") //el point donde voy a hacer las peticiones
 // //libreria de manejor de archivos
@@ -14,21 +14,21 @@ const resolvers = require("./lib/resolvers") //traer un archivo js con una funci
  const app = express(); //llamando el servicio configurando un server
  const port = process.eventNames.port || 3000; //debo darle un puerto para escuchar las peticiones
 
- const schema = buildSchema(
-    readFileSync(
-        join(__dirname, 'lib', 'schema.graphql'), //ruta donde esta el archivo 
-        'utf-8'
-    )
-);
+//  const schema = buildSchema(
+//     readFileSync(
+//         join(__dirname, 'lib', 'schema.graphql'), //ruta donde esta el archivo 
+//         'utf-8'
+//     )
+// );
 
 
-// const typeDefs = readFileSync(
-//     join(__dirname, 'lib', 'schema.graphql'),
-//     'utf-8'
-// )
-// //llamo a la dependencia
-// const schema = makeExecutableSchema({
-//     typeDefs, resolvers})
+const typeDefs = readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+)
+//llamo a la dependencia
+const schema = makeExecutableSchema({
+    typeDefs, resolvers})
 
 // //configurar la aplicacion para cuando se lance, la ruta relativa, llamo al middleware y
 // // pasamos los tres argumentos: schema, rootValue, graphiql (aplicacion grafixa)
