a3b2a69f9c1666c95bd70244ffb8d80c9cd0f98f
Manuel Alejandro Jojoa <manueljojoa7@gmail.com>
2021-12-12 15:09:48 -0500

    correcion de errores

--- treact-api/index.js
+++ treact-api/index.js
@@ -1,35 +1,26 @@
-'use strict'
+"use strict";
 
 require('dotenv').config()
-const { graphql, buildSchema } = require('graphql') // primer  tipo de consulta squema
-const { makeExecutableSchema } = require('graphql-tools')
-const express = require('express')
-const cors = require('cors')
-const { graphqlHTTP } = require('express-graphql') // el point donde voy a hacer las peticiones
+const {makeExecutableSchema}= require('graphql-tools') 
+const express = require("express") 
+const cors = require ('cors')
+const { graphqlHTTP } = require("express-graphql") //el point donde voy a hacer las peticiones
 // //libreria de manejor de archivos
-const { readFileSync } = require('fs')
-const { join } = require('path')
-const resolvers = require('./lib/resolvers') // traer un archivo js con una funcion
+ const {readFileSync} = require("fs") 
+const {join} = require("path")
+const resolvers = require("./lib/resolvers") //traer un archivo js con una funcion
 
-const app = express() // llamando el servicio configurando un server
-const port = process.eventNames.port || 3000 // debo darle un puerto para escuchar las peticiones
-const isDev = process.env.NODE_ENV !== 'production'
 
-//  const schema = buildSchema(
-//     readFileSync(
-//         join(__dirname, 'lib', 'schema.graphql'), //ruta donde esta el archivo
-//         'utf-8'
-//     )
-// );
+ const app = express()  //llamando el servicio configurando un server
+ const port = process.env.port || 3000  //debo darle un puerto para escuchar las peticiones
+ const isDev = process.env.NODE_ENV !== 'production'
 
 const typeDefs = readFileSync(
-  join(__dirname, 'lib', 'schema.graphql'),
-  'utf-8'
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
 )
-// llamo a la dependencia
-const schema = makeExecutableSchema({
-  typeDefs, resolvers
-})
+//llamo a la dependencia
+const schema = makeExecutableSchema({typeDefs, resolvers})
 
 // //configurar la aplicacion para cuando se lance, la ruta relativa, llamo al middleware y
 // // pasamos los tres argumentos: schema, rootValue, graphiql (aplicacion grafixa)
@@ -37,11 +28,12 @@ const schema = makeExecutableSchema({
 app.use(cors())
 
 app.use('/api', graphqlHTTP({
-  schema: schema,
-  rootValue: resolvers,
-  graphiql: true
+    schema: schema, 
+    rootValue: resolvers,
+    graphiql: isDev
 }))
 
+
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}/api`)
+    console.log(`Server is listening at http://localhost:${port}/api`)
 })
--- treact-api/lib/resolvers.js
+++ treact-api/lib/resolvers.js
@@ -1,78 +1,11 @@
 'use strict'
 
-// const Usuarios=[
-//   {
-//       _id:"1",
-//       nombre: "manuel",
-//       apellido: "jojoa",
-//       identificacion:"106177168",
-//       correo:"manuel@gmail.com",
-//       contrasena:"ajdhsuodjs",
-//       rol:"administrador",
-//       estado:"activo"
-//   },
-//   {
+const mutations = require('./mutations')
+const queries = require('./queries')
 
-//       nombre: "johana",
-//       apellido: "argoty",
-//       identificacion:"106177187",
-//       correo:"johana@gmail.com",
-//       contrasena:"ajdhsuodjs",
-//       rol:"estudiante",
-//       estado:"activo"
-//   }
-// ]
 
-// module.exports={
-//   Query:{
-
-//       getUsuarios:()=>{
-//           return Usuarios
-//       },
-//       getUsuario:(root, args)=>{
-//           const usuario= Usuarios.filter(usuario=>usuario._id == args.id)
-//           return usuario.pop()
-
-//       }
-//   }
-
-// }
-
-const connectDb = require('./db')
-const { ObjectId } = require('mongodb')
-
-module.exports = {
-  Query: {
-
-    getUsuarios: async () => {
-      let db
-      let usuarios = []
-      try {
-        db = await connectDb()
-        usuarios = await db.collection('usuarios').find().toArray()
-      } catch (error) {
-        console.error(error)
-      }
-      return usuarios
-    },
-    getUsuario: async (root, { id }) => { // id es un objeto
-      let db
-      let usuario
-      try {
-        db = await connectDb()
-        usuario = await db.collection('usuarios').findOne({ _id: ObjectId(id) })
-      } catch (error) {
-        console.error(error)
-      }
-      return usuario
-    }
-  }
-
+module.exports={
+    Query: queries,
+    Mutation: mutations  
 }
 
-// getUsuario:(root, args)=>{
-//     const usuario= Usuario.filter(usuario=>usuario._id === args.id)
-//     return usuario.pop()
-
-// }
-// npm es un gestor de paquetes
