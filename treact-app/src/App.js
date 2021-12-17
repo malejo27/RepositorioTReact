67f3bdf6da58ef52b689b13e3719631069edec5d
“estephany2828” <ejargoty@unicauca.edu.co>
2021-12-16 22:20:41 -0500

    front crud de usuarios

--- treact-app/src/App.js
+++ treact-app/src/App.js
@@ -1,37 +1,69 @@
-
-
 import React from 'react';
-import ListUsuarios from "./components/usuarios/listaUsuarios";
+import ListUsuarios from "./components/usuarios/ListaUsuarios";
 import {Col, Container, Row} from 'reactstrap';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import 'boostrap/dist/css/boostrap.min.css';
+import {Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter} from 'reactstrap';
 
-
-
-import './App.css';
+//import './App.css';
 import CrearUsaurio from './components/usuarios/createUsuario';
 import Footer from './components/shared/Footer';
 
+const data =[
 
+]
 function App() {
-  // const [usuarios, setUsuarios]= useState([]);
+   const [usuarios, setUsuarios]= useState([]);
 
-  // const cargarUsuarios= () =>{
-  //   axios.get('http://localhost:8080')
-  //   .then(({data}) => setUsuarios(data));
-  // }
-  // useEffect(cargarUsuarios, []) 
+   const cargarUsuarios= () =>{
+     axios.get('http://localhost:8080/usuarios')
+     .then(({data}) => setUsuarios(data));
+   }
+   useEffect(cargarUsuarios, []);
 
+   const onSubmit = (values)=>{
+     if (tarea){
+      axios.put(`http://localhost:8080/usuarios/${usuario.id}`, values)
+      .then(()=>cargarUsuarios());
+  
+     }else{
+      axios.post('http://localhost:8080/usuarios', values)
+      .then(()=>{
+        setTarea();
+        cargarUsuarios();
+        
+      });
+     }
+    
+   }
+
+   const eliminarUsuario = (usuario) => {
+    axios.delete(`http://localhost:8080/usuarios/${usuario.id}`)
+    .then(()=>cargarUsuarios());
+
+   }
+   const editarUsuario = (usuario) => {
+    axios.put(`http://localhost:8080/usuarios/${usuario.id}`)
+    .then(()=>cargarUsuarios());
+
+   }
+   
+   const mapDispatchToProps = {
+     
+   }
+   
   return (
     <>
    
     <Container>
-      <Row>
+      <Banner/>
+      <Row className="mt-5">
         <Col md={6}>
-        <ListUsuarios ></ListUsuarios>
+        <ListUsuarios usuarios={usuarios} onDelete={eliminarUsuario} onEdit= {(usuario)=> setUsuarios()} />
         </Col>
         <Col  md={6}>
-        <CrearUsaurio />
+        <CrearUsaurio onSubmit={onSubmit} usuario= {usuario}/>
         </Col>
       </Row>
       <Footer></Footer>
